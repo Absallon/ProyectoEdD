e7b046e6d20abd8e59368aeb0f5be44e6475bde5
Absallon <131483583+Absallon@users.noreply.github.com>
2023-11-28 21:42:36 -0600

    Main con Prim
    Le agregue el Prim al main, agregue en forma de comentarios el vertice inicializado con los otros valores dentro del archivo de texto y quite la parte de comentarios innecesaria.

--- main.cpp
+++ main.cpp
@@ -6,50 +6,22 @@
 using namespace std;
 
 int main(){
-    Grafo g("SubestacionesCFE.txt");
-    //Vertice v("Queretaro");
-    g.muestraListaVertices();
-    g.muestraMatrizAdj();
-    //vertice de partida
-    string verticePartida="EzequelMontes";
-    Vertice v(verticePartida);
-    //g.Prim(verticePartida);
-
-
-
-    
-    /*g.busquedaProf(v);
-    g.muestraListaVertices();
-    g.recorridos();
     /*
-    string s;
-    int numVertices=0;
-    int **matAdj;
-    //Crea objeto para abrir archivo de lectura
-    ifstream arch("prueba.txt");
-    //ifstream arch("prueba1.txt"); //Probar con un archivo que no existe
-    if(arch.fail()){
-        cout << "Error al abrir el archivo..."<<endl;
-        exit(1);
-    }
-    
-    //Leer el numero de vertices
-    getline(arch,s);
-    numVertices = stoi(s);
-    matAdj = new int*[numVertices];
-    //Crear las columnas para cada renglon
-
-    //Leer información del archivo
-    while(!arch.eof()){ //Leer todo el archivo
-        getline(arch,s);
-        //arch >> s; //Muestra palabra por palabra debido a delimitadores
-        cout <<s<<endl;
-    }
-    /*getline(arch,s);
-    cout <<"Cadena leida: "<<s<<endl;
-    getline(arch,s);
-    cout <<"Cadena leida: "<<s<<endl; * /
-    arch.close(); //Cerrar el archivo
+    ---PROYECTO---
     */
+    Grafo CFE("SubestacionesCFE.txt");
+    //Definicion del vertice de Partida en Ezequiel Montes
+    Vertice vPartida("EzequielMontes");
+    //Vertices de prueba
+    //Vertice vPartida("ApaseoCelaya");
+    //Vertice vPartida("VillasDelCimatario");
+    //Vertice vPartida("BuenaVista");
+    //Vertice vPartida("SEPoniente");
+    //Vertice vPartida("Laborcilla");
+    //Vertice vPartida("Tepeyac");
+    CFE.muestraListaVertices();
+    CFE.muestraMatrizAdj();
+    CFE.Prim(vPartida);
+    CFE.muestraListaVertices();
     return 0;
 }
